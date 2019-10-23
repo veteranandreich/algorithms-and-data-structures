d46da6786936a3bd2d6d3373005ce4b14e7663b0
Vladimir <vladimir27goncharov@icloud.com>
2019-10-24 00:54:35 +0300

    Find and Push_Back methods added

--- SinglyLinkedList.cpp
+++ SinglyLinkedList.cpp
@@ -8,39 +8,87 @@ struct Element
 };
 
 template <typename type>
-class SinglyLinkedList 
+class SinglyLinkedList
 {
-	private:
-		Element <type> *top = new Element <type>;
+private:
+	Element <type>* top = new Element <type>;
+	Element <type>* bottom = new Element <type>;
+	void Push_first(const type& element)
+	{
+		Element <type>* new_element = new Element <type>;
+		new_element->value = element;
+		bottom->next = new_element;
+		new_element->next = bottom;
+		top->next = new_element;
+	}
 
-	public: 
-		void Push(const type& element)
+public:
+	void Push(const type& element)
+	{
+		if (top->next == nullptr)
 		{
-			Element <type>* a = new Element <type>;
-			a->next = top->next;
-			a->value = element;
-			top->next = a;
-		};
+			Push_first(element);
+		}
+		else
+		{
+			Element <type>* new_element = new Element <type>;
+			new_element->value = element;
+			new_element->next = top->next;
+			top->next = new_element;
+		}
+	};
 
-		void Print()
+	void Print()
+	{
+		Element <type>* element = top->next;
+		while (element != bottom)
 		{
-			Element <type> *element = top->next;
-			while (element != nullptr) {
-				std::cout << element->value << std::endl;;
-				element = element->next;
-			}
-		};
+			std::cout << element->value << std::endl;
+			element = element->next;
+		}
+	};
+
+	Element<type>* Find(const type& target)
+	{
+		Element <type>* element = top->next;
+		while (element != bottom)
+		{
+			if (element->value == target) return element;
+			element = element->next;
+		}
+
+		return nullptr;
+	}
+
+	void Push_back(const type& element)
+	{
+		if (top->next == nullptr)
+		{
+			Push_first(element);
+		}
+		else
+		{
+			Element <type>* new_element = new Element <type>;
+			new_element->value = element;
+			new_element->next = bottom;
+			bottom->next->next = new_element;
+		}
+	}
+
+
 };
 
 int main()
 {
-	/*
+	/* 
 	EXAMPLE:
 	SinglyLinkedList <int> list;
 	list.Push(5);
 	list.Push(6);
 	list.Push(7);
-	list.Print(); 
+	list.Push_back(8);
+	list.Print();
+	Element <int>* b = list.Find(5);
+	if (b != nullptr) std::cout << b->value; 
 	*/
 }
-
