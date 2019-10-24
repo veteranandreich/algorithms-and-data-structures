59bd51b60e41b13fb7b11271b156bf78ac68bbb4
Vladimir <vladimir27goncharov@icloud.com>
2019-10-24 22:54:52 +0300

    Insert, Erase, Pop, Begin and End methonds added

--- SinglyLinkedList.cpp
+++ SinglyLinkedList.cpp
@@ -13,13 +13,14 @@ class SinglyLinkedList
 private:
 	Element <type>* top = new Element <type>;
 	Element <type>* bottom = new Element <type>;
+	size_t list_size = 0;
 	void Push_first(const type& element)
 	{
 		Element <type>* new_element = new Element <type>;
 		new_element->value = element;
 		bottom->next = new_element;
-		new_element->next = bottom;
 		top->next = new_element;
+		list_size++;
 	}
 
 public:
@@ -35,23 +36,24 @@ public:
 			new_element->value = element;
 			new_element->next = top->next;
 			top->next = new_element;
+			list_size++;
 		}
-	};
+	}
 
 	void Print()
 	{
 		Element <type>* element = top->next;
-		while (element != bottom)
+		while (element != nullptr)
 		{
 			std::cout << element->value << std::endl;
 			element = element->next;
 		}
-	};
+	}
 
 	Element<type>* Find(const type& target)
 	{
 		Element <type>* element = top->next;
-		while (element != bottom)
+		while (element != nullptr)
 		{
 			if (element->value == target) return element;
 			element = element->next;
@@ -70,25 +72,71 @@ public:
 		{
 			Element <type>* new_element = new Element <type>;
 			new_element->value = element;
-			new_element->next = bottom;
 			bottom->next->next = new_element;
+			bottom->next = new_element;
+			list_size++;
 		}
 	}
 
+	void Insert(Element<type>* previous_element, const type& element)
+	{
+		Element <type>* new_element = new Element <type>;
+		new_element->value = element;
+		if (previous_element->next == nullptr) bottom->next = new_element;
+		else new_element->next = previous_element->next;
+		previous_element->next = new_element;
+		list_size++;
+	}
+
+	void Erase(Element<type>* previous_element)
+	{
+		if (previous_element->next != nullptr)
+		{
+			Element<type>* _ = previous_element->next->next;
+			delete previous_element->next;
+			previous_element->next = _;
+			list_size--;
+		}
+	}
+
+	Element<type>* Pop()
+	{
+		Element<type>* _ = top->next;
+		top->next = top->next->next;
+		list_size--;
+		return _;
+	}
+
+	size_t Size()
+	{
+		return list_size;
+	}
+
+	Element<type>* Begin()
+	{
+		return top->next;
+	}
+
+	Element<type>* End()
+	{
+		return bottom->next;
+	}
 
 };
 
 int main()
 {
+	//Example Program:
 	/* 
-	EXAMPLE:
 	SinglyLinkedList <int> list;
 	list.Push(5);
-	list.Push(6);
-	list.Push(7);
 	list.Push_back(8);
-	list.Print();
 	Element <int>* b = list.Find(5);
-	if (b != nullptr) std::cout << b->value; 
+	list.Insert(b, 6);
+	list.Insert(b, 7);
+	for (Element <int>* b = list.Begin(); b != nullptr; b = b->next)
+	{
+		std::cout << b->value<< std::endl;
+	}
 	*/
 }
